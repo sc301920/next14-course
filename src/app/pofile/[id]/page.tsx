7f322257649f2006958d67d32b58bd14283915a3
kelai <kelai@17weiju.com>
2023-11-12 20:31:49 +0800

    +1

--- src/app/pofile/[id]/page.tsx
+++ src/app/pofile/[id]/page.tsx
@@ -1,11 +1,12 @@
 "use client";
-import { useParams, useRouter,  } from 'next/navigation'
+import { useParams, useRouter, useSearchParams  } from 'next/navigation'
 export default function ProfileID(){
     const parmas = useParams()
     const router = useRouter()
+    const searchParams = useSearchParams()
     return (
         <div>
-            <h1>my ProfileID page {parmas.id}</h1>
+            <h1>my ProfileID page {parmas.id} {searchParams.get('userName')}</h1>
             <div onClick={()=>{router.push('/pofile')}}>back to main page</div>
         </div>
     )
